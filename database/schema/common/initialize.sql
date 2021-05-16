59b548b8098279d97f4bf8b5c314f86635daab14
vigilantloki <dpramanik.pidilite@gmail.com>
2021-05-16 14:34:13 +0530

    🩹 add currency tables in common/initialize.sql

--- database/schema/common/initialize.sql
+++ database/schema/common/initialize.sql
@@ -1,7 +1,9 @@
 \echo 'Creating Common Table(s) ...'
 
 \i database/schema/common/tables/country.sql
+\i database/schema/common/tables/currency.sql
 
 \echo 'Populating with Initial Seed Data ...'
 
 \i database/schema/common/seed/country.sql
+\i database/schema/common/seed/currency.sql
