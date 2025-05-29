9b0dfbdb63c7c410ad7f59dfbd3cc3e73c5a5394
E33605 <debmalyapramanik.005@gmail.com>
2025-05-29 12:44:36 +0530

    🎉 create new function to report missing currency

--- database/schema/dev/functions/forex_rate.sql
+++ database/schema/dev/functions/forex_rate.sql
@@ -8,6 +8,10 @@ common schema as ``common.forex_rate_tx``. The function(s) are:
     * ``dev.missing_date_in_forex_udf`` - To find the missing dates
         in the forex rates table, which is useful for auditing purpose.
 
+    * ``dev.missing_currency_in_forex_udf`` - To find the missing
+        currencies in the forex rates table, which is defined under
+        the currency master ``common.currency_mw`` table.
+
 Copywright © [2025] Debmalya Pramanik, DigitPhilia INC.
 ********************************************************************/
 
@@ -28,6 +32,34 @@ BEGIN
     ) AS dates
     WHERE dates NOT IN (SELECT DISTINCT effective_date FROM common.forex_rate_tx)
     ORDER BY dates DESC;
+
+END; $$
+LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION dev.missing_currency_in_forex_udf ()
+RETURNS TABLE (
+    missing_currency CHAR(3)
+    , missing_currency_name VARCHAR(64)
+    , missing_currency_type CHAR(1)
+    , missing_currency_subtype CHAR(1)
+) AS $$
+
+BEGIN
+    RETURN QUERY
+
+    SELECT
+        currency_code AS missing_currency
+        , currency_name AS missing_currency_name
+        , currency_type AS missing_currency_type
+        , currency_subtype AS missing_currency_subtype
+    FROM common.currency_mw
+    WHERE currency_code NOT IN (
+        SELECT
+            DISTINCT target_currency_code
+        FROM common.forex_rate_tx
+    )
+    ORDER BY currency_code;
 
 END; $$
 LANGUAGE 'plpgsql';
