4a667ca06e36799facd5f48f64e867bad237706b
vigilantloki <dpramanik.pidilite@gmail.com>
2026-02-07 15:10:11 +0530

    🩹 patch forex table to macrodb schema
    - refactor from digitphilia/dataway@8a081e33df8ae5df8bc10625f505549c46d17849 to be part of macrodb

--- database/schema/common/forex_rate.sql
+++ database/schema/common/forex_rate.sql
@@ -3,10 +3,9 @@ The Foreign Exchange Rates (FOREX) Tracking Table
 
 The foreign exchange rates sourced from different vendors/data source
 providers are tracked in this table and is a common table to one/more
-projects in the system. The schema considers data source proxy to the
-internal data source schema.
-
-Copywright © [2025] Debmalya Pramanik, DigitPhilia INC.
+projects in the system. The table is created to keep track of the
+exchange rates that can be provided from multiple sources - check
+data sources for more information.
 ********************************************************************/
 
 CREATE TABLE IF NOT EXISTS common.forex_rate_tx (
@@ -21,25 +20,30 @@ CREATE TABLE IF NOT EXISTS common.forex_rate_tx (
         CONSTRAINT fk_base_currency_code
             REFERENCES common.currency_mw(currency_code)
             ON UPDATE CASCADE
-            ON DELETE SET NULL,
+            ON DELETE RESTRICT,
 
     target_currency_code
         CHAR(3) NOT NULL
         CONSTRAINT fk_target_currency_code
             REFERENCES common.currency_mw(currency_code)
             ON UPDATE CASCADE
-            ON DELETE SET NULL,
+            ON DELETE RESTRICT,
 
     exchange_rate
         NUMERIC(19, 6) NOT NULL,
 
     data_source_proxy_id
         CHAR(4) NOT NULL
-        CONSTRAINT fk_forex_data_source_proxy_id
-            REFERENCES public.data_source_proxy_mw(data_source_proxy_id)
+        CONSTRAINT fk_forex_data_source_id
+            REFERENCES public.data_source_mw(data_source_id)
             ON UPDATE CASCADE
-            ON DELETE SET NULL,
+            ON DELETE RESTRICT,
 
     CONSTRAINT uq_forex_value_from_source
-        UNIQUE (effective_date, base_currency_code, target_currency_code, data_source_proxy_id)
+        UNIQUE (
+            effective_date
+            , base_currency_code
+            , target_currency_code
+            , data_source_id
+        )
 );
