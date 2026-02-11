384dc6747198f0cda64a693da7ab4611acecc2c0
vigilantloki <dpramanik.pidilite@gmail.com>
2026-02-11 14:05:20 +0530

    💣🛠️ fix function definition, improve reducibility - reduce function name, were more than 30 chars long - fix data parameters and default values - updated function note

--- database/schema/common/functions/forex_rate.sql
+++ database/schema/common/functions/forex_rate.sql
@@ -3,7 +3,15 @@ Get the FOREX Rate(s) for a Date Range from a desired Data Source
 
 The FOREX Rate(s) are stored under the ``common.forex_rate_tx`` table
 and the function consideres input parameters - date range and data
-source name (internal private name) to fetch the data.
+source name (internal private name, check ``public.data_source_mw``)
+to fetch the data. A two part function is developed to return the
+data, which are as follows:
+
+    * ``common.forex_from_src_for_range_udf`` - simply return the
+        underlying table in a ordered format for a given data source.
+
+    * ``common.forex_from_src_for_range_base_udf`` - return the
+        forex data in a desired base currency.
 
 The user defined dynamic function is customizable and is accepts the
 following parameters:
@@ -13,21 +21,19 @@ following parameters:
     * ``p_final_date`` - The final date of the range (default is 
         current date),
     * ``p_base_currency_code`` - The base currency code (default is
-        INR),
-    * ``p_date_source_proxy_id`` - The data source proxy ID which is
-        internal private name of the data source (default is 1).
+        INR, i.e. Indian Rupees),
+    * ``p_date_source_id`` - The data source proxy ID which is
+        internal private name of the data source (default is 'ERAPI').
 
-For simplicity purpose, the function(s) are named with "source" short
+For simplicity purpose, the function(s) are named with "src" short
 for "data source" and "range" for the "date range", to reduce the
 length of the function name.
-
-Copywright © [2025] Debmalya Pramanik, DigitPhilia INC.
 ********************************************************************/
 
-CREATE OR REPLACE FUNCTION common.forex_from_source_for_range_udf (
+CREATE OR REPLACE FUNCTION common.forex_from_src_for_range_udf (
     p_start_date DATE DEFAULT (CURRENT_DATE - INTERVAL '365 D')::DATE,
     p_final_date DATE DEFAULT CURRENT_DATE,
-    p_date_source_proxy_id public.data_source_proxy_id DEFAULT 'R001'
+    p_date_source_id CHAR(5) DEFAULT 'ERAPI'
 
 ) RETURNS TABLE (
     effective_date DATE,
@@ -47,7 +53,7 @@ BEGIN
     FROM common.forex_rate_tx AS forex
     WHERE
         forex.effective_date BETWEEN p_start_date AND p_final_date
-        AND forex.data_source_proxy_id = p_date_source_proxy_id
+        AND forex.data_source_id = p_date_source_id
     ORDER BY
         forex.effective_date DESC
         , forex.base_currency_code
@@ -57,10 +63,10 @@ END; $$
 LANGUAGE 'plpgsql';
 
 
-CREATE OR REPLACE FUNCTION common.forex_from_source_for_range_base_udf (
+CREATE OR REPLACE FUNCTION common.forex_from_src_for_range_base_udf (
     p_start_date DATE DEFAULT (CURRENT_DATE - INTERVAL '365 D')::DATE,
     p_final_date DATE DEFAULT CURRENT_DATE,
-    p_date_source_proxy_id public.data_source_proxy_id DEFAULT 'R001',
+    p_date_source_id CHAR(5) DEFAULT 'ERAPI',
     p_base_currency_code CHAR(3) DEFAULT 'INR'
 
 ) RETURNS TABLE (
@@ -78,15 +84,15 @@ BEGIN
         , rtbl.target_currency_code AS base_currency_code
         , ltbl.target_currency_code AS target_currency_code
         , (ltbl.exchange_rate / rtbl.exchange_rate)::NUMERIC(19, 6) AS exchange_rate
-    FROM common.forex_from_source_for_range_udf(
+    FROM common.forex_from_src_for_range_udf(
         p_start_date
         , p_final_date
-        , p_date_source_proxy_id
+        , p_date_source_id
     ) ltbl
-    JOIN common.forex_from_source_for_range_udf(
+    JOIN common.forex_from_src_for_range_udf(
         p_start_date
         , p_final_date
-        , p_date_source_proxy_id
+        , p_date_source_id
     ) rtbl ON ltbl.effective_date = rtbl.effective_date
     WHERE
         rtbl.target_currency_code = p_base_currency_code
