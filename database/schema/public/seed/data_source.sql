07c9323c775867abf44136045c3aaf341d048e34
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2026-03-01 21:35:29 +0530

    ✨ added new data source 'Our World in Data'

--- database/schema/public/seed/data_source.sql
+++ database/schema/public/seed/data_source.sql
@@ -1,4 +1,5 @@
 INSERT INTO public.data_source_mw (data_source_id, data_source_name, data_source_desc, data_source_uri) VALUES
     ('ERAPI', 'Exchange Rates API', 'Historical and Real-Time Exchange Rates', 'https://exchangeratesapi.io/'),
     ('XECCV', 'XE Currency Converter', 'Live Exchange Rates for Personal & Business', 'https://www.xe.com/currencyconverter/'),
-    ('OSEXR', 'Open Source Exchange Rates API', 'Currency API Data at a Granular Level', 'https://openexchangerates.org/');
+    ('OSEXR', 'Open Source Exchange Rates API', 'Currency API Data at a Granular Level', 'https://openexchangerates.org/'),
+    ('OWIDT', 'Our World in Data', 'Oxford University Affiliated, Open Data on Global Problems like Health, Hunger, etc.', 'https://ourworldindata.org/');
