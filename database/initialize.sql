a7331b39de0e3ea4a0588a7fbfaf97392f030390
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2026-05-20 21:14:21 +0530

    🐛 fixed order of initialization of schemas: public → common → dev

--- database/initialize.sql
+++ database/initialize.sql
@@ -2,10 +2,10 @@
 Configure and Initialize a PostgreSQL Macroeconomics Database
 
 The macroeconomics database (macrodb) is a structured data of various
-factors like - currency, inflation etc. that can be used to analyze a
+factors like currency, inflation, etc., that can be used to analyze a
 country's performance using different indicators. The database is
-created independently thus providing seamless access to other forward
-looking models keeping the core structure intact.
+created independently, thus providing seamless access to other forward
+looking at models, keeping the core structure intact.
 
 Copyright © [2021] Debmalya Pramanik (ZenithClown), AivenIO DBA
 ********************************************************************/
@@ -22,6 +22,8 @@ CREATE SCHEMA IF NOT EXISTS
 SQL Table Schema Execution Order - STRICT Follow
 ********************************************************************/
 
-\i database/schema/dev/initialize.sql
 \i database/schema/public/initialize.sql
 \i database/schema/common/initialize.sql
+
+-- ? Initialize "dev" schema at the end; it contains table mapping
+\i database/schema/dev/initialize.sql
